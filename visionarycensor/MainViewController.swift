0c4162b6a7841f6273c23fb1b0c57ff70d32d599
Mosdevv <vikentorpierre@gmail.com>
2018-07-22 17:10:12 -0400

    first commit

--- visionarycensor/MainViewController.swift
+++ visionarycensor/MainViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupLayout()
+    }
+    func setupLayout(){
+        view.backgroundColor = .red
+        
     }
 
     override func didReceiveMemoryWarning() {

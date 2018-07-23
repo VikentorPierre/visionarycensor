5dd76015aa96ecbbac302a141c91de116a4ace8a
Mosdevv <vikentorpierre@gmail.com>
2018-07-23 19:31:54 -0400

    added the toggle button for the camera

--- visionarycensor/Controller/MainViewController.swift
+++ visionarycensor/Controller/MainViewController.swift
@@ -7,29 +7,64 @@
 //
 
 import UIKit
-import MobileCoreServices
+import AVFoundation // using this to work with AVFoundationCapture
 import SnapKit
 
-class MainViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MainViewController: UIViewController {
     
+    // 1. declare a gloable captureSession object
+    var myCaptureSession = AVCaptureSession()
+    var backCamera: AVCaptureDevice?
+    var fontCamera: AVCaptureDevice?
+    var currentCamera: AVCaptureDevice?
+    var newCameraInput: AVCaptureDeviceInput? // toggle
+    var newDevicePosition: AVCaptureDevice? // toggle
     
+    var photoOutput: AVCapturePhotoOutput?
     
+    // the preview before taking a photo
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
     
+    // this variable stored the final picture after you click the takePhoto button
+    var theFinalPictureFromCamera: UIImage?
+    
+//
+//    var toggleCameraPosition: UITapGestureRecognizer = {
+//        let obj = UITapGestureRecognizer(target: self, action: #selector(toggleCamera))
+//        obj.numberOfTapsRequired = 2
+//        obj.numberOfTouchesRequired = 2
+//
+//        return obj
+//    }()
+    
+
     
     let takeButton:UIButton = {
         let obj = UIButton()
         obj.backgroundColor = .white
-//        obj.setTitle("Button", for: .normal)
-//        obj.setTitleColor(.blue, for: .normal)
         obj.layer.cornerRadius = 30
         obj.clipsToBounds = true
+        obj.addTarget(self, action: #selector(showPreview), for: .touchUpInside)
         return obj
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCaptureSession()
+        setupCaptureDevice()
+        setupInputOutput()
+        setupPreviewLayer()
+        startRunningCaptureSession()
+        
+        let toggle: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleCamera))
+        toggle.numberOfTapsRequired = 2
+        
+        view.addGestureRecognizer(toggle)
+
+    }
+    override func loadView() {
+        super.loadView()
         setupLayout()
-//        VicksetupNavigationBar()
     }
     func setupLayout(){
         view.backgroundColor = .black
@@ -40,28 +75,115 @@ class MainViewController: UIViewController, UIImagePickerControllerDelegate, UIN
             make.bottom.equalTo(-20)
         }
         
-
+    }
+    
+    //2. second goto:setupCaptureSession method and config the session
+    // settting the goodness of the resolution of the photo
+    
+    fileprivate func setupCaptureSession(){
+        self.myCaptureSession.sessionPreset = AVCaptureSession.Preset.photo
+    }
+    
+    // 3. config the device set the type of media
+    // set the back and font and current camera and set the default camera position
+    
+    fileprivate func setupCaptureDevice(){
         
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: .video, position: AVCaptureDevice.Position.unspecified)
+        let devices = deviceDiscoverySession.devices
         
+        for device in devices{
+            if device.position == AVCaptureDevice.Position.back{
+                self.backCamera = device
+            }else if device.position == AVCaptureDevice.Position.front{
+                self.fontCamera = device
+            }
+        }
+        self.currentCamera = self.backCamera
+    }
+    
+    // 4.
+    fileprivate func setupInputOutput(){
+        do{
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
+            photoOutput = AVCapturePhotoOutput()
+            
+            myCaptureSession.addInput(captureDeviceInput)
+            
+            photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey:AVVideoCodecType.jpeg])], completionHandler: nil)
+            myCaptureSession.addOutput(photoOutput!)
+        }catch{
+            print(error)
+        }
+    }
+    
+    // 5. config the cameraPreview and setting the layer frame than inserting that layer into our view sublayer
+    fileprivate func setupPreviewLayer(){
+        self.cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: self.myCaptureSession)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        cameraPreviewLayer?.frame = self.view.frame
+        self.view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+    }
+    
+    // final: 
+    fileprivate func  startRunningCaptureSession(){
+        self.myCaptureSession.startRunning()
+    }
+    
+    @objc func showPreview(){
+        let setting = AVCapturePhotoSettings()
+        self.photoOutput?.capturePhoto(with: setting, delegate: self)
+    }
+    
+    @objc func toggleCamera(){
+        //print(" gester works ")
+        self.myCaptureSession.beginConfiguration()
+
+        newDevicePosition = (self.currentCamera?.position == AVCaptureDevice.Position.back) ? self.fontCamera: self.backCamera
+        for input in myCaptureSession.inputs {
+            myCaptureSession.removeInput(input as! AVCaptureDeviceInput)
+        }
+        do{
+            newCameraInput = try AVCaptureDeviceInput(device: newDevicePosition!)
+        }catch let err{ print(err)}
+
+        if myCaptureSession.canAddInput(newCameraInput!){ myCaptureSession.addInput(newCameraInput!)}
+
+        currentCamera = newDevicePosition
+        myCaptureSession.commitConfiguration()
     }
 
-    func VicksetupNavigationBar(){
-
-
-        navigationItem.title = "Camera"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(lanuchCamera))
-     
-    }
-    @objc func lanuchCamera(){
-        let vc = CameraHelper()
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+}
 
+// Camera photo Delegate
+extension MainViewController: AVCapturePhotoCaptureDelegate{
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let imageData = photo.fileDataRepresentation(){
+            
+            theFinalPictureFromCamera = UIImage(data: imageData)
 
+//            let vc = Preview(nibName: "Preview", bundle: nil)
+//
+//            vc.contentView.image = theFinalPictureFromCamera
+//            self.present(vc, animated: true, completion: nil)
+            
+            
+            let vc = PreviewViewController()
+            self.theFinalPictureFromCamera = UIImage(data: imageData)
+
+            vc.content.image = theFinalPictureFromCamera
+            self.present(vc, animated: true)
+            
+        }
+    }
 }
 
--- visionarycensor/Controller/PreviewViewController.swift
+++ visionarycensor/Controller/PreviewViewController.swift
@@ -8,16 +8,24 @@
 
 import UIKit
 import SnapKit
+import AVFoundation // using this to work with AVFoundationCapture
 
 class PreviewViewController: UIViewController {
     
-    let content:UIImageView = {
-        let obj = UIImageView()
+//
+    let cancelButton: UIButton = {
+        let obj = UIButton()
+        obj.setTitle("Cancel", for: .normal)
+
+        obj.setTitleColor( .red, for: .normal)
+   
+        obj.addTarget(self, action: #selector(cancelbutton), for: .touchUpInside)
         return obj
     }()
-    let subContentView:UIView = {
-        let obj = UIView()
-        obj.backgroundColor = UIColor(named: .init(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.5))
+    
+    let content:UIImageView = {
+        let obj = UIImageView()
+        obj.backgroundColor = .blue
         return obj
     }()
 
@@ -27,35 +35,31 @@ class PreviewViewController: UIViewController {
     override func loadView() {
         super.loadView()
         setupLayout()
+
     }
     func setupLayout(){
         self.view.addSubview(content)
-        content.addSubview(subContent)
+        view.addSubview(cancelButton)
+        
+        //content.addSubview(cancelButton)
         content.snp.makeConstraints { (make) in
             make.edges.equalToSuperview()
         }
-        subContent.snp.makeConstraints { (make) in
-            make.leadingMargin.topMargin.trailingMargin.equalToSuperview()
-            make.height.equalTo(60)
+        cancelButton.snp.makeConstraints { (make) in
+            make.leading.top.equalTo(50)
         }
-        
-        
+    }
+    
+    
+    @objc func cancelbutton(){
+        self.dismiss(animated: true, completion: nil)
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
